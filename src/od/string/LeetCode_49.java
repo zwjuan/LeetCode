cf26f879dc30a074c5862a7ad875347ab8ccad84
zwjuan <1442304303@qq.com>
2023-09-06 10:54:01 +0800

    [fix]修改49
    Signed-off-by: zwjuan <1442304303@qq.com>

--- src/od/string/LeetCode_49.java
+++ src/od/string/LeetCode_49.java
@@ -1,5 +1,50 @@
 package od.string;
 
-public class LeetCode_49 {
+import java.util.*;
 
+/**
+ * 49. 字母异位词分组 -
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ *
+ * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+ *
+ * 解法1： 对字符串进行排序
+ *
+ * 示例 1:
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * 示例 2:
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ *
+ * 示例 3:
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ *
+ */
+public class LeetCode_49 {
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+
+        System.out.println(groupAnagrams(new String[]{""}));
+
+        System.out.println(groupAnagrams(new String[]{"a"}));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] tmp = s.toCharArray();
+            Arrays.sort(tmp);
+
+            String key = new String(tmp);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(s);
+            map.put(key, list);
+        }
+
+        return new ArrayList<>(map.values());
+     }
 }
